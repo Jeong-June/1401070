97a71fda3a9002c1966cdd01e0c1636628f68bd4
Lee Jeong June <hackerjune@naver.com>
2014-03-08 19:55:57 +0900

    1 line print

--- World/World/Program.cs
+++ World/World/Program.cs
@@ -7,8 +7,6 @@ namespace World
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("안녕하세요!");
-			Console.WriteLine ("141070 이정준 입니다.");
-			Console.WriteLine ("정호영 교수님 프연 실습중");
 		}
 	}
 }
